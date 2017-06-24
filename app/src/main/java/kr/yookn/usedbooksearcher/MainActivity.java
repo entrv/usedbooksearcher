d39cab7940af992a834b3d24fd0d66a14128c21f
entrvn7 <entrvn7@gmail.com>
2017-06-24 22:14:32 +0900

    test

---
+++ app/src/main/java/kr/yookn/usedbooksearcher/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.yookn.usedbooksearcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
